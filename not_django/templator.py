5d45c95195e31b5f3b44d99868d603ce8af92ad9
mrfuu04 <mrfuu04@gmail.com>
2023-02-09 22:02:47 +0300

    реализовал наследование шаблонов

--- not_django/templator.py
+++ not_django/templator.py
@@ -1,19 +1,23 @@
 import os
 from jinja2 import Template
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
 
 from not_django.application import BASE_DIR
 
 
-def render(template_name, request, context=None):
+def render(template_name, request, context: dict = None):
     """
     Рендерер страницы.
     :param template_name: Имя шаблона
-    :param kwargs: Контекст передаваемый в шаблон
+    :param context: Контекст передаваемый в шаблон
     :return:
     """
+    env = Environment()
     view_module = request['view_module']
-    template_path = os.path.join(BASE_DIR, view_module, 'templates', template_name)
-    with open(template_path, encoding='utf-8') as f:
-        template = Template(f.read())
+    template_path = os.path.join(BASE_DIR, view_module, 'templates')
+
+    env.loader = FileSystemLoader(template_path)
+    template = env.get_template(template_name)
 
     return template.render(context)
